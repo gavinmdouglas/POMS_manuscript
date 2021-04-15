1831754b791ce3cb87a19b57f3ee225055ee2d22
Gavin Douglas <gavinmdouglas@gmail.com>
2021-04-15 13:59:10 -0300

    Clean up main figure

--- scripts/display_items/Maintext_MAG.based_sims_prop_sig_and_ranking_alt_tools.R
+++ scripts/display_items/Maintext_MAG.based_sims_prop_sig_and_ranking_alt_tools.R
@@ -4,6 +4,7 @@ library(cowplot)
 library(ggExtra)
 library(ggplot2)
 library(ggpubr)
+library(ggbeeswarm)
 
 setwd("~/github_repos/POMS_manuscript/data/intermediates/MAG.based_simulations/")
 
@@ -15,13 +16,64 @@ func_rand_summary_POMS_wilcoxon.musicc <- readRDS(file = "simulation_summaries/f
 func_rand_summary_POMS_wilcoxon.relab_limma_voom <- readRDS(file = "simulation_summaries/func_rand_summary_POMS_wilcoxon.relab_limma_voom.rds")
 func_rand_summary_POMS_aldex2_deseq2 <- readRDS(file = "simulation_summaries/func_rand_summary_POMS_aldex2_deseq2.rds")
 
-# Create dataframe with combined proportion of significant genes for each tool / simulation type.
+qual_col <- c("#1f78b4",
+              "firebrick1",
+              "gold2",
+              "darkslategray3",
+              "mediumorchid",
+              "palegreen")
+
+# Now get rankings for each tool (only for Focal gene simulations).
+MAG.based_all_rank <- data.frame(Tool = c(rep("POMS", 1000),
+                                          
+                                          rep("Wilcoxon test\n(corrected)", 1000),
+                                          
+                                          rep("Wilcoxon test\n(relab.)", 1000),
+                                          
+                                          rep("limma-voom", 1000),
+                                          
+                                          rep("ALDEx2", 10),
+                                          
+                                          rep("DESeq2", 10)),
+                                 
+                                 rank = c(func_rand_summary_POMS_wilcoxon.musicc$POMS_rank_0.25,
+                                          
+                                          func_rand_summary_POMS_wilcoxon.musicc$wilcoxon.musicc_rank_0.001,
+                                          
+                                          func_rand_summary_POMS_wilcoxon.relab_limma_voom$wilcoxon.relab_rank_0.001,
+                                          
+                                          func_rand_summary_POMS_wilcoxon.relab_limma_voom$limma.voom_rank_0.001,
+                                          
+                                          func_rand_summary_POMS_aldex2_deseq2$aldex2_rank_0.001,
+                                          
+                                          func_rand_summary_POMS_aldex2_deseq2$deseq2_rank_0.001))
+
+
+MAG.based_all_rank$Tool <- factor(MAG.based_all_rank$Tool, 
+                                  levels = c("POMS", "ALDEx2", "DESeq2", "limma-voom", "Wilcoxon test\n(corrected)", "Wilcoxon test\n(relab.)"))
+
+MAG.based_all_rank$Fill <- rep("Differential abundance approach", nrow(MAG.based_all_rank))
+MAG.based_all_rank[which(MAG.based_all_rank$Tool == "POMS"), "Fill"] <- "Balance tree-based approach"
+
+
+
+
+MAG.based_all_rank_boxplots <- ggplot(MAG.based_all_rank, aes(x = Tool, y = rank, fill = Tool)) +
+                                        geom_quasirandom(color = "grey") +
+                                        geom_boxplot(outlier.shape = NA, alpha = 0.5, lwd = 1) +
+                                        theme_bw() +
+                                        ylab("Focal gene ranking\n(lower is better)") +
+                                        xlab("") +
+                                        scale_fill_manual(values = qual_col) +
+                                        theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
+                                        theme(legend.position = "none") 
+
 
 MAG.based_all_prop_sig <- data.frame(Tool = c(rep("POMS", 2000),
                                               
-                                              rep("Wilcoxon test (MUSiCC)", 2000),
+                                              rep("Wilcoxon test\n(corrected)", 2000),
                                               
-                                              rep("Wilcoxon test (relab.)", 2000),
+                                              rep("Wilcoxon test\n(relab.)", 2000),
                                               
                                               rep("limma-voom", 2000),
                                               
@@ -73,80 +125,35 @@ MAG.based_all_prop_sig$Simulation <- factor(MAG.based_all_prop_sig$Simulation,
                                             levels = c("Random taxa", "Focal gene"))
 
 MAG.based_all_prop_sig$Tool <- factor(MAG.based_all_prop_sig$Tool, 
-                                            levels = c("POMS", "ALDEx2", "DESeq2", "limma-voom", "Wilcoxon test (MUSiCC)", "Wilcoxon test (relab.)"))
+                                            levels = c("POMS", "ALDEx2", "DESeq2", "limma-voom", "Wilcoxon test\n(corrected)", "Wilcoxon test\n(relab.)"))
 
-MAG.based_all_prop_sig_boxplots <- ggplot(MAG.based_all_prop_sig, aes(x = Tool, y = prop, fill = Simulation)) +
-                                          geom_boxplot() +
+MAG.based_all_prop_sig_boxplots <- ggplot(MAG.based_all_prop_sig, aes(x = Tool, y = prop, fill = Tool)) +
+                                          geom_quasirandom(color = "grey") +
+                                          geom_boxplot(outlier.shape = NA, alpha = 0.5, lwd = 1) +
+                                          facet_grid( ~ Simulation) +
                                           theme_bw() +
                                           ylim(0, 1) +
                                           ylab("Proportion sig. gene families") +
-                                          scale_fill_manual(values = c("skyblue3", "coral2")) +
-                                          theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+                                          xlab("") +
+                                          scale_fill_manual(values = qual_col) +
+                                          theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
+                                          theme(legend.position = "none")
 
+MAG.based_alt_tool_comparisons <- plot_grid(MAG.based_all_rank_boxplots,
+                                            MAG.based_all_prop_sig_boxplots,
+                                            nrow = 1, ncol = 2, labels = c('a', 'b'), rel_widths = c(1, 1.75))
 
-
-
-
-# Now get rankings for each tool (only for Focal gene simulations).
-MAG.based_all_rank <- data.frame(Tool = c(rep("POMS", 1000),
-                                              
-                                              rep("Wilcoxon test (MUSiCC)", 1000),
-                                              
-                                              rep("Wilcoxon test (relab.)", 1000),
-                                              
-                                              rep("limma-voom", 1000),
-                                              
-                                              rep("ALDEx2", 10),
-                                              
-                                              rep("DESeq2", 10)),
-                                     
-                                     rank = c(func_rand_summary_POMS_wilcoxon.musicc$POMS_rank_0.25,
-                                              
-                                              func_rand_summary_POMS_wilcoxon.musicc$wilcoxon.musicc_rank_0.001,
-                                              
-                                              func_rand_summary_POMS_wilcoxon.relab_limma_voom$wilcoxon.relab_rank_0.001,
-                                              
-                                              func_rand_summary_POMS_wilcoxon.relab_limma_voom$limma.voom_rank_0.001,
-                                              
-                                              func_rand_summary_POMS_aldex2_deseq2$aldex2_rank_0.001,
-                                              
-                                              func_rand_summary_POMS_aldex2_deseq2$deseq2_rank_0.001))
-
-
-MAG.based_all_rank$Tool <- factor(MAG.based_all_rank$Tool, 
-                                  levels = c("POMS", "ALDEx2", "DESeq2", "limma-voom", "Wilcoxon test (MUSiCC)", "Wilcoxon test (relab.)"))
-
-MAG.based_all_rank$Fill <- rep("Differential abundance approach", nrow(MAG.based_all_rank))
-MAG.based_all_rank[which(MAG.based_all_rank$Tool == "POMS"), "Fill"] <- "Balance tree-based approach"
-
-
-
-
-MAG.based_all_rank_boxplots <- ggplot(MAG.based_all_rank, aes(x = Tool, y = rank, fill = Fill)) +
-                                      geom_boxplot() +
-                                      theme_bw() +
-                                      ylab("Focal gene ranking") +
-                                      scale_fill_manual(values = c("steelblue4", "springgreen4")) +
-                                      theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
-                                      theme(legend.position = "none")
-
-
-
-MAG.based_alt_tool_comparisons <- plot_grid(MAG.based_all_prop_sig_boxplots,
-                                            MAG.based_all_rank_boxplots,
-                                            nrow = 1, ncol = 2, labels = c('a', 'b'), rel_widths = c(2.5, 1))
-
-ggsave(filename = "~/github_repos/POMS_manuscript/figures/Maintext_MAG.based_sims_prop_sig_and_ranking_alt_tools.pdf",
+ggsave(filename = "~/github_repos/POMS_manuscript/display_items/Maintext_MAG.based_sims_prop_sig_and_ranking_alt_tools.pdf",
        plot = MAG.based_alt_tool_comparisons,
        device = "pdf",
-       width = 6,
-       height = 4,
+       width = 10,
+       height = 5,
        dpi = 600)
        
        
-ggsave(filename = "~/github_repos/POMS_manuscript/figures/Maintext_MAG.based_sims_prop_sig_and_ranking_alt_tools.png",
+ggsave(filename = "~/github_repos/POMS_manuscript/display_items/Maintext_MAG.based_sims_prop_sig_and_ranking_alt_tools.png",
        plot = MAG.based_alt_tool_comparisons,
        device = "png",
-       width = 6,
-       height = 4,
+       width = 10,
+       height = 5,
        dpi = 300)
