33e92e617ee0a0589abff9234b234c41a7523007
Gavin Douglas <gavinmdouglas@gmail.com>
2022-12-11 18:46:06 -0500

    Delete update_R_package.R

--- scripts/update_R_package.R
+++
@@ -1,9 +0,0 @@
-### Code for re-generating POMS R package files whenever there has been additional functions added or 
-### changes to man pages.
-
-library(devtools)
-library(roxygen2)
-
-setwd("~/github_repos/POMS/")
-devtools::document()
-devtools::load_all(path = "~/github_repos/POMS/")
